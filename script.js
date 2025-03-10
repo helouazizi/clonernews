8a0525058d8828998721efc99a698abcd12e85a2
heeemzaaa <hamzaelkhawlani00@gmail.com>
2025-03-10 22:34:12 +0000

    handle the dead posts and deleted comments , and undefined posts

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function loadContent(nbOfCards) {
         fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
             .then(response => response.json())
             .then(data => {
-                if (data.type === "comment") {
+                let status = data.type === 'story' || data.type === 'poll' || data.type === 'job'
+                if (data.type === "comment" || (status && data.dead) || data.title === undefined) {
                     loadContent(1)
                     return
                 }
@@ -67,7 +68,7 @@ function createCards(data) {
         <div class="title"><a href="#" id="story-title">${data.title}</a></div>
         <div class="info">
                     by <span id="story-author">${data.by}</span> | Score: <span id="story-score">${data.score}</span> | Comments: <span
-                        id="story-comments">${data.kids ? data.kids.length : 0}</span> <span id="type">${data.type}</span>
+                        id="story-comments">${data.kids ? data.kids.length : 0}</span> | type: <span id="type">${data.type}</span>
         </div>
         <div class="comments">
         </div>
@@ -75,7 +76,6 @@ function createCards(data) {
 
     div.addEventListener('click', e => {
         getPostInfos(div.dataset['idPost'])
-        // divInfos.style.display = 'block'
     })
 
     return div
@@ -86,12 +86,11 @@ function getPostInfos(idPost) {
     fetch(`https://hacker-news.firebaseio.com/v0/item/${idPost}.json`)
         .then(response => response.json())
         .then(data => {
-
             divInfos.innerHTML = `
                 <div class="title"><a href="#" id="story-title">${data.title}</a></div>
                 <div class="info">
                     by <span id="story-author">${data.by}</span> | Score: <span id="story-score">${data.score}</span> | Comments: <span
-                    id="story-comments">${data.descendants}</span> <span id="type">${data.type}</span>
+                    id="story-comments">${data.descendants}</span> | type: <span id="type">${data.type}</span>
                 </div>
                 <div id="comments">
                     <h2>COMMENTS</h2>
@@ -117,8 +116,9 @@ function getComments(idsComment) {
             .then(data => {
                 let cmts = document.querySelector('#comments')
                 let cmt = document.createElement('div')
-                cmt.innerHTML = data.by + "<br><br>" + data.text
-
+                if (!cmt.deleted) {
+                    cmt.innerHTML = data.by + "<br><br>" + data.text
+                }
                 cmts.append(cmt)
             })
     }
